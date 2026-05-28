51ee0a36ea35b515f06eb75b7a8522c5f92c1a5c
arkadii <arkadii.personal@proton.me>
2026-05-29 00:29:35 +0200

    Update px4_drone.cpp

---
+++ src/px4_drone.cpp
@@ -0,0 +1,25 @@
+#include "px4_drone.hpp"
+
+void Px4Drone::Init() {
+
+}
+
+void Px4Drone::LaunchMission(const std::any& mission_items) {
+
+}
+
+void Px4Drone::Kill() {
+
+}
+
+void Px4Drone::Arm() {
+
+}
+
+void Px4Drone::Disarm() {
+
+}
+
+std::any Px4Drone::GetTelemetry() {
+    return 0;
+}
