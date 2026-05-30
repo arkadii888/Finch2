442bba60317ba37a45074caad1f682bb56480b3b
arkadii888 <arkadii.personal@proton.me>
2026-05-31 00:56:18 +0200

    prompt secured

--- src/agent.cpp
+++ src/agent.cpp
@@ -17,6 +17,5 @@ void Agent::Run() {
 };
 
 void Agent::SetPrompt(const std::string& prompt) {
-    std::lock_guard<std::mutex> lock {prompt_mutex_};
-    prompt_ = prompt;
+    prompt_.Set(prompt);
 }
--- src/agent.hpp
+++ src/agent.hpp
@@ -5,6 +5,23 @@
 
 #include "drone.hpp"
 
+class PromptString {
+ public:
+    std::string Get() const {
+        std::lock_guard<std::mutex> lock {mutex_};
+        return value_;
+    }
+
+    void Set(const std::string& value) {
+        std::lock_guard<std::mutex> lock {mutex_};
+        value_ = value;
+    }
+
+ private:
+    mutable std::mutex mutex_;
+    std::string value_;
+};
+
 class Agent {
  public:
     Agent(Drone& drone);
@@ -14,6 +31,5 @@ class Agent {
 
  private:
     Drone& drone_;
-    std::string prompt_;
-    std::mutex prompt_mutex_;
+    PromptString prompt_;
 };
