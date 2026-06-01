9e987acd9db80c55233c5983de50f5cc2af1e940
arkadii <arkadii.personal@proton.me>
2026-06-01 14:51:27 +0200

    model path

--- src/config.hpp
+++ src/config.hpp
@@ -2,10 +2,14 @@
 
 #include <string>
 
+#ifndef PROJECT_ROOT_DIR
+#error "PROJECT_ROOT_DIR is not defined!"
+#endif
+
 struct Config {
     const int http_server_port = 8888;
     const int llama_server_port = 8889;
-    const std::string model_path = "/home/arkadii/Documents/Projects/CxxProjects/Finch2/models/gemma-4-E2B-it-Q4_K_M.gguf";
+    const std::string model_path = std::string {PROJECT_ROOT_DIR} + "/models/gemma-4-E2B-it-Q4_K_M.gguf";
 };
 
 inline Config config;
