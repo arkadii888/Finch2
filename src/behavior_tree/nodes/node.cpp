890a4e14ea1a15617d593374d9ffdf759c4698af
arkadii888 <arkadii.personal@proton.me>
2026-07-06 14:09:59 +0200

    Update node.cpp

--- src/behavior_tree/nodes/node.cpp
+++ src/behavior_tree/nodes/node.cpp
@@ -7,10 +7,6 @@ void Node::AddChild(std::unique_ptr<Node> child) {
     childrens_.push_back(std::move(child));
 }
 
-void Node::SetStatus(NodeStatus status) {
-    status_ = status;
-}
-
 const std::vector<std::unique_ptr<Node>>& Node::GetChildrens() const {
     return childrens_;
 }
