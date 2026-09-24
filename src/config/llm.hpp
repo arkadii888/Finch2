1875fc915e6835dd6dfeefa7e9d87e6e4d8bf33c
Arkadii <arkadii.personal@proton.me>
2026-09-24 15:22:07 +0200

    sorted lines

--- src/config/llm.hpp
+++ src/config/llm.hpp
@@ -3,23 +3,23 @@
 #include <filesystem>
 
 struct LlmConfig {
+    bool llama_jinja {true};
+    double llama_temperature {0.0};
+    double map_half_window_m {1'000.0};
+    int llama_context_size {16'384};
+    int llama_image_tokens {2'304};
+    int llama_max_tokens {6'000};
+    int map_zoom {15};
+    int max_go_to {26};
+    int renderer_timeout_s {60};
     std::filesystem::path btree_grammar_path;
     std::filesystem::path dem_path;
     std::filesystem::path inference_log_dir;
-    int llama_context_size {16'384};
-    int llama_image_tokens {2'304};
-    bool llama_jinja {true};
-    int llama_max_tokens {6'000};
-    double llama_temperature {0.0};
-    double map_half_window_m {1'000.0};
     std::filesystem::path map_path;
-    int map_zoom {15};
-    int max_go_to {26};
     std::filesystem::path mmproj_path;
     std::filesystem::path model_path;
     std::filesystem::path python_path;
     std::filesystem::path renderer_path;
-    int renderer_timeout_s {60};
 
     static LlmConfig Make();
 
